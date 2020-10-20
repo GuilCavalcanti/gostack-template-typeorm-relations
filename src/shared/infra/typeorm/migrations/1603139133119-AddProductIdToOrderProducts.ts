d3e8a7c0de734cdc7ce6ef8266c71bf2d5825087
GuilCavalcanti <58377529+GuilCavalcanti@users.noreply.github.com>
2020-10-20 22:50:08 +0100

    Done

---
+++ src/shared/infra/typeorm/migrations/1603139133119-AddProductIdToOrderProducts.ts
@@ -0,0 +1,35 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddProductIdToOrderProducts1603139133119
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrderProductsID',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrderProductsID');
+    await queryRunner.dropColumn('orders_products', 'product_id');
+  }
+}
